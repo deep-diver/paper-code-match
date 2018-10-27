692167eb1d547f2d1174b741fc2d5565532cdbfa
chansung park <deep.diver.csp@gmail.com>
2018-10-27 09:35:21 +0900

    adding cancel button on the context menu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-// import VisualTOC from './diagrams/VisualTOC.html';
-
-// eagerly initialize vtoc  as it's above the fold
-// const tocNav = document.getElementById('vtoc');
-// const visualTOC = new VisualTOC({target: tocNav});
-
-// lazily initialize any diagram below the fold. E.G:
-// {
-//   const figure = document.getElementById('StyleTransferExamples');
-//   figure.addEventListener("ready", function() {
-//     const styleTransferExamples = new StyleTransferExamples({target: figure});
-//   });
-// }
-
 var selectionEndTimeout = null;
 var range = null;
 
@@ -20,24 +6,21 @@ let menuVisible = false;
 
 const toggleMenu = command => {
     console.log(command);
-  menu.style.display = command === "show" ? "block" : "none";
-  menuVisible = !menuVisible;
+    menu.style.display = command === "show" ? "block" : "none";
+    menuVisible = !menuVisible;
 };
 
 const setPosition = ({ top, left }) => {
-  menu.style.left = `${left}px`;
-  menu.style.top = `${top}px`;
-  toggleMenu("show");
+    menu.style.left = `${left}px`;
+    menu.style.top = `${top}px`;
+    toggleMenu("show");
 };
 
 window.onload = function () {
-    window.addEventListener("contextmenu", e => {
-        e.preventDefault();
-    });
-
     document.onmousedown = function(e) {
         if(menuVisible) {
             toggleMenu("hide");
+            menuSelectionEnabled = false;
         }
     };
 
@@ -49,6 +32,13 @@ window.onload = function () {
         span.className = 'highlight';
         span.appendChild(range.extractContents());
         range.insertNode(span);
+
+        toggleMenu("hide");
+    });
+
+    $("#cancel").on('mousedown', function(e) {
+        e.preventDefault();
+        window.getSelection().empty();
 
         toggleMenu("hide");
     });
@@ -66,36 +56,21 @@ function userSelectionChanged(e) {
     }
 
     selectionEndTimeout = setTimeout(function () {
-        // document.ondblclick = function(e) {
-        //     let selectedTagName = document.getSelection().anchorNode.parentElement.tagName;
-        
-        //     if (selectedTagName.toLowerCase() == "p") {
-        //         range = document.getSelection().getRangeAt(0);
-                
-        //         if (range.toString().trim() != "") {
-        //             const origin = {
-        //                 left: e.pageX,
-        //                 top: e.pageY
-        //             };
-
-        //             setPosition(origin);
-        //         }
-        //     }
-        // };
-
         document.onmouseup = function(e) {
-            let selectedTagName = document.getSelection().anchorNode.parentElement.tagName;
-        
-            if (selectedTagName.toLowerCase() == "p") {
-                range = document.getSelection().getRangeAt(0);
+            if (document.getSelection().anchorNode) {
+                let selectedTagName = document.getSelection().anchorNode.parentElement.tagName;
             
-                if (range.toString().trim() != "") {
-                    const origin = {
-                        left: e.pageX,
-                        top: e.pageY
-                    };
-                    
-                    setPosition(origin);
+                if (selectedTagName.toLowerCase() == "p") {
+                    range = document.getSelection().getRangeAt(0);
+                
+                    if (range.toString().trim() != "") {
+                        const origin = {
+                            left: e.pageX,
+                            top: e.pageY
+                        };
+                        
+                        setPosition(origin);
+                    }
                 }
             }
         };
